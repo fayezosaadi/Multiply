443f1dc8bf6179e676bbddc87e62dad9d682fa32
fayezosaadi <fayezosaadi@gmail.com>
2019-09-15 21:53:04 -0700

    add test case for zero

--- multiply.js
+++ multiply.js
@@ -1,5 +1,5 @@
 const multiply = (value, multiplyConstant = 321) => {
-  if (multiplyConstant == 0) return 0;
+  if (multiplyConstant === 0) return 0;
 
   /* Add value one by one */
   if (multiplyConstant > 0)
--- test.js
+++ test.js
@@ -2,6 +2,11 @@ const assert = require("assert");
 
 const multiply = require("./multiply");
 
-it("should return 963 when given value equals 3", () => {
-  assert.equal(multiply(3), 963);
+describe("Multiply", () => {
+  it("should return 0", () => {
+    assert.equal(multiply(0), 0);
+  })
+  it("should return 963 when given value equals 3", () => {
+    assert.equal(multiply(3), 963);
+  });
 });
