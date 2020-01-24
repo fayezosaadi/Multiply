ad9b4fa4d8a00327dc23501a8fc513e78abcd34e
fayezosaadi <fayezosaadi@gmail.com>
2020-01-24 13:01:06 -0800

    add unit tests for negative values

--- multiply.js
+++ multiply.js
@@ -2,8 +2,9 @@ const multiply = (value, multiplyConstant = 321) => {
   if (multiplyConstant === 0) return 0;
 
   /* Add value one by one */
-  if (multiplyConstant > 0)
+  if (multiplyConstant > 0) {
     return value + multiply(value, multiplyConstant - 1);
+  }
 };
 
 module.exports = multiply;
--- test.js
+++ test.js
@@ -9,4 +9,8 @@ describe("Multiply", () => {
   it("should return 963 when given value equals 3", () => {
     assert.equal(multiply(3), 963);
   });
+
+  it("should return -642 when given value equals -2", () => {
+    assert.equal(multiply(-2), -642);
+  });
 });
